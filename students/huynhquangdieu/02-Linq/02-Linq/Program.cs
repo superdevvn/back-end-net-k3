8adc178c4ea16c121bb362be7cb2931938ae195e
huynhquangdieu <huynhquangdieu0404@gmail.com.vn>
2018-06-17 09:59:47 +0700

    -Init

---
+++ students/huynhquangdieu/02-Linq/02-Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Linq
+{
+    class Program
+    {
+        static List<Student> students = new List<Student>();
+        static void Main(string[] args)
+        {
+        }
+        static void Init()
+        {
+            students.Add(new Student()
+            {
+                Code = "001",
+                Name = "N1",
+                IsMale = false,
+            });
+        }
+    }
+}
