a12f31bc52c966f5bfaff146f78d1f69f2c45796
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-06-24 09:44:39 +0700

    - Sample Code First

---
+++ source demo/04-Code First/Models/Common/SampleDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Common
+{
+    public class SampleDbContext : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+
+        public SampleDbContext() : base("Data Source=DESKTOP-15KKVN2\\SQLEXPRESS;Initial Catalog=SampleCodeFirst;Integrated Security=True")
+        { }
+    }
+}
---
+++ source demo/04-Code First/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Role
+    {
+        // guid uuid
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
